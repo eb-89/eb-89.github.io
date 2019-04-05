1735235ccc3780f25b8c7bc4468302c20ec01634
Erik Bertse <erik_bertse@hotmail.com>
2019-04-05 12:13:21 +0200

    basic layout

---
+++ nav/circlenav.js
@@ -0,0 +1,8 @@
+function changeText () {
+	elm = document.getElementById("mainbodyholder");
+	
+	// while (elm.firstChild) {
+ //  			elm.removeChild(elm.firstChild);
+	// }
+	// $('#mainbodyholder').load("/cv.html");
+}
